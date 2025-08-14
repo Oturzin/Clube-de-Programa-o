825960f9383de4d2cea31e24b3fcf104fb434921
Oturzin <arthur.pacheco@loyola.g12.br>
2025-08-14 15:00:10 -0300

    Create aula 1.py

---
+++ aula 1.py
@@ -0,0 +1,21 @@
+nome = 'arthur'
+idade = 12
+turma = 'E'
+print(nome)
+print(idade)
+print(turma)
+print("meu nome e " + nome)
+print("eu sou da turma " + turma)
+print("eu tenho " + str (idade) + "anos")
+print()
+print()
+
+num4 = 5
+resultado = num1 + num2 + num3 + num4
+resultado2 = resultado /4
+print(resultado2)
+num1 = 10
+num2 = 10
+
+
+num3 = 5
