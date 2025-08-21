87c762b294f8de85769eef9b267e41c5f5f7e247
Oturzin <arthur.pacheco@loyola.g12.br>
2025-08-21 15:00:34 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,20 @@
+print("Hello World")
+nota1 = input('digite a primeira nota:')
+nota2 = input('digite a segunda nota:')
+nota3 = input('digite a terceira nota:')
+nota4 = input('digite a quarta nota:')
+
+nota1 = int(nota1)
+nota2 = int(nota2)
+nota3 = int(nota3)
+nota4 = int(nota4)
+
+
+
+
+
+result=  (nota1 + nota2 + nota3 + nota4)/4
+
+
+if result >= 6 : print('ele e um genio nao tem como')
+if result <= 5 : print('ixi ja era ksksks')
