40b65940b0cebcb14bd4dd2b59d2f8daacfa84ef
Oturzin <arthur.pacheco@loyola.g12.br>
2025-08-28 14:15:08 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,21 @@
+
+nota1 = inprimeira nota:')
+nota2 = input('digite a segunda nota:')
+nota3 = input('digite a terceira nota:')
+nota4 = input('digite a quarta nota:')
+
+nota1 = int(nota1)
+nota2 = int(nota2)
+nota3 = int(nota3)
+nota4 = int(nota4)
+
+
+
+
+
+result=  (nota1 + nota2 + nota3 + nota4)/4
+
+
+if result >= 6 and result <=10: print('ele e um genio nao tem como') 
+if result <= 5 and result >=0: print('ixi ja era ksksks')
+if result >=5 and result <6 :print('reprovado')ut('digite a p
